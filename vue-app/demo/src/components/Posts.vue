cc346e941ef24d7a79bbede11eacbddd08eb9f4f
niawjunior <pasupol@dome.cloud>
2018-05-25 01:34:25 +0700

    Using Single File Vue components

--- vue-app/demo/src/components/Posts.vue
+++ vue-app/demo/src/components/Posts.vue
@@ -1,12 +1,37 @@
 <template>
-    <p>HELLO</p>    
+    <div class="container">
+    <table class="table table-striped mt-4">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Id</th>
+        <th scope="col">Title</th>
+        <th scope="col">Body</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="post in posts" :key="post.id">
+        <th scope="row"></th>
+        <td>{{ post.id }}</td>
+        <td>{{ post.title }}</td>
+        <td>{{ post.body }}</td>
+        </tr>
+    </tbody>
+    </table>    
+  </div>
 </template>
 <script>
     export default {
         name: 'PostsExample',
         data() {
             return {
+                posts: [
+                    {id: 1, title: 'Post 1', body: 'Body 1'},
+                    {id: 2, title: 'Post 2', body: 'Body 2'},
+                    {id: 3, title: 'Post 3', body: 'Body 3'},
+                    {id: 4, title: 'Post 4', body: 'Body 4'},
 
+                ]
             }
         }
     }
