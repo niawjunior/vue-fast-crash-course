e46e520d4ba33209117e498ce9582a09036b8bf5
niawjunior <pasupol@dome.cloud>
2018-05-25 02:47:49 +0700

    Routing

--- vue-app/demo/src/main.js
+++ vue-app/demo/src/main.js
@@ -2,12 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import Posts from './components/Posts'
 
+Vue.use(VueRouter);
+const routes = [
+  {
+    path: '/', component: Posts
+  }
+];
+
+const router = new VueRouter({routes: routes})
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+/* eslint-disable o-new */
 new Vue({
   el: '#app',
+  router,
   components: { App },
   template: '<App/>'
 })
