1b5f680c440cde9f800758a1d6c4f16baaeb700e
niawjunior <pasupol@dome.cloud>
2018-05-25 02:18:54 +0700

    get data from api | jsonplaceholder

--- vue-app/demo/src/components/Posts.vue
+++ vue-app/demo/src/components/Posts.vue
@@ -3,16 +3,16 @@
     <table class="table table-striped mt-4">
     <thead>
         <tr>
-        <th scope="col">#</th>
         <th scope="col">Id</th>
+        <th scope="col">User Id</th>
         <th scope="col">Title</th>
         <th scope="col">Body</th>
         </tr>
     </thead>
     <tbody>
         <tr v-for="post in posts" :key="post.id">
-        <th scope="row"></th>
         <td>{{ post.id }}</td>
+        <th>{{ post.userId }}</th>
         <td>{{ post.title }}</td>
         <td>{{ post.body }}</td>
         </tr>
@@ -21,18 +21,20 @@
   </div>
 </template>
 <script>
+    import axios from 'axios'
     export default {
         name: 'PostsExample',
         data() {
             return {
                 posts: [
-                    {id: 1, title: 'Post 1', body: 'Body 1'},
-                    {id: 2, title: 'Post 2', body: 'Body 2'},
-                    {id: 3, title: 'Post 3', body: 'Body 3'},
-                    {id: 4, title: 'Post 4', body: 'Body 4'},
 
                 ]
             }
+        },
+        created() {
+            axios.get('https://jsonplaceholder.typicode.com/posts').then(posts => {
+                this.posts = posts.data
+            })
         }
     }
 </script>
